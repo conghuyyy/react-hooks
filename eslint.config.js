13e478c5331f7267b57083d7e70239d2ec9372e4
Cong Huy <conghuy.work@gmail.com>
2024-10-20 10:23:26 +0700

    feat: Update eslint & typescript config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,18 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tsESLint from 'typescript-eslint';
+import { configs as typescriptESLintConfigs } from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
 import pluginImport from 'eslint-plugin-import';
 import pluginUnusedImports from 'eslint-plugin-unused-imports';
 import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import typescriptParser from '@typescript-eslint/parser';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.browser, parser: typescriptParser } },
   { ...pluginJs.configs.recommended, name: 'javascript-plugin' },
-  ...tsESLint.configs.recommended,
+  ...typescriptESLintConfigs.recommended,
   { ...pluginReact.configs.flat.recommended, name: 'react-plugin' },
   { plugins: { 'react-hooks': pluginReactHooks }, name: 'react-hooks-plugin' },
   { ...pluginImport.flatConfigs.recommended, name: 'import-plugin' },
@@ -41,6 +42,35 @@ export default [
       'import/resolver': {
         typescript: { alwaysTryTypes: true, project: './tsconfig.json' },
       },
+    },
+  },
+  {
+    name: 'custom-rule-configuration',
+    rules: {
+      'no-console': 'error',
+      'max-params': ['error', 3],
+      'max-depth': ['error', 4],
+      'max-statements': ['error', 30],
+      'max-statements-per-line': ['error', { max: 1 }],
+      'max-lines': [
+        'error',
+        { max: 300, skipBlankLines: true, skipComments: true },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'index',
+            'sibling',
+            'parent',
+            'object',
+            'type',
+          ],
+        },
+      ],
     },
   },
 ];
